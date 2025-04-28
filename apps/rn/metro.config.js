87e8c765eef24253c26fb3b4a576797ea14d3eb9
Brad Anderson <brad@sankatygroup.com>
2025-04-28 09:11:06 -0400

    wip: RN app imports broken

--- apps/rn/metro.config.js
+++ apps/rn/metro.config.js
@@ -1,21 +1,36 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// metro.config.js
 const path = require('path');
+const { makeMetroConfig } = require('@rnx-kit/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
 
-const root = path.resolve(__dirname, '..', '..', 'node_modules');
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {
-  resolver: {
-    nodeModulesPaths: [
-      root,
-    ],
-    unstable_enablePackageExports: true,
-  },
-  watchFolders: [root],
+// Define workspace root
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+const libSrc = path.resolve(workspaceRoot, 'lib', 'src');
+
+// Add packages paths
+const extraNodeModules = {
+  modules: path.resolve(workspaceRoot, 'node_modules'),
+  lib: libSrc,
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const watchFolders = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+  libSrc,
+];
+
+const nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+module.exports = makeMetroConfig({
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+    extraNodeModules,
+    nodeModulesPaths,
+    sourceExts: ['mjs', 'js', 'json', 'ts', 'tsx'],
+  },
+  watchFolders,
+});
